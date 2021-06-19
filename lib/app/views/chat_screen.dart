e7e242599b65e4cfbc9b86c7afdda71e09138f82
esequias.m.oliveira <esequias.m.oliveira@gmail.com>
2021-06-19 15:54:24 -0300

    Add login with google account

--- lib/app/views/chat_screen.dart
+++ lib/app/views/chat_screen.dart
@@ -2,8 +2,10 @@ import 'dart:io';
 
 import 'package:chat/app/components/text_composer.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
@@ -11,8 +13,55 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final GoogleSignIn googlSignIn = GoogleSignIn();
+
+  User _correntUser;
+
+  @override
+  void initState() {
+    super.initState();
+    this._correntUser = FirebaseAuth.instance.currentUser;
+  }
+
+  Future<User> _getUser() async {
+    if (_correntUser != null) return _correntUser;
+
+    try {
+      // Trigger the Google Authentication flow.
+      final GoogleSignInAccount signInAccount = await googlSignIn.signIn();
+      // Obtain the auth details from the request.
+      final GoogleSignInAuthentication authentication =
+          await signInAccount.authentication;
+      // Create a new credential.
+      final GoogleAuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: authentication.accessToken,
+        idToken: authentication.idToken,
+      );
+
+      // Sign in to Firebase with the Google [UserCredential].
+      final UserCredential googleUserCredential =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+
+      return googleUserCredential.user;
+    } catch (error) {
+      return null;
+    }
+  }
+
   void _sendMessage({String text, File imageFile}) async {
-    Map<String, dynamic> data = {};
+    final User user = await _getUser();
+    if (user == null) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text("Não foi possivel fazer o login. Tente novamente!'"),
+        backgroundColor: Colors.red,
+      ));
+    }
+
+    Map<String, dynamic> data = {
+      "uid": user.uid,
+      "senderName": user.displayName,
+      "senderPhotoUrl": user.photoURL
+    };
 
     if (imageFile != null) {
       UploadTask task = FirebaseStorage.instance
